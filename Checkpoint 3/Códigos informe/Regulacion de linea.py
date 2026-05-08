f23662f7ec8c7181c0a02b50212ae7c4da7f9c60
Lautaro García Vitale <lagarciav@fi.uba.ar>
2026-05-08 12:06:25 -0300

    Modif. Reg_Lin

--- Checkpoint 3/Códigos informe/Regulacion de linea.py
+++ Checkpoint 3/Códigos informe/Regulacion de linea.py
@@ -13,9 +13,9 @@ def leer_txt_ltspice(nombre_archivo):
 # ============================================================
 # 1) REGULACIÓN DE LÍNEA: Vo vs Vreg
 # ============================================================
-Vreg_simu, Vo_simu = leer_txt_ltspice("RegLin_simu.txt")
+Vreg_simu, Vo_simu = leer_txt_ltspice("/Users/lautarogarciavitale/Desktop/UBA/Materias/CIRCUITOS 2/taller-de-circuitos-grupo-5/Checkpoint 3/Códigos informe/RegLin_simu.txt")
 
-df = pd.read_csv("RegLin_medida.csv")
+df = pd.read_csv("/Users/lautarogarciavitale/Desktop/UBA/Materias/CIRCUITOS 2/taller-de-circuitos-grupo-5/Checkpoint 3/Códigos informe/RegLin_medida.csv")
 
 # reemplaza coma decimal por punto
 df["Vreg"] = df["Vreg"].astype(str).str.replace(",", ".")
@@ -34,18 +34,18 @@ Vo_medida   = df["Vo"].to_numpy()
 
 plt.figure()
 
-plt.plot(Vreg_simu, Vo_simu[:, 0], color = 'black', label='Simulación', linestyle='--')
-plt.plot(Vreg_medida, Vo_medida, color = 'blue', label='Medición')
+plt.plot(Vreg_simu, Vo_simu[:, 0], color = 'grey', label='Simulación', linestyle='--')
+plt.plot(Vreg_medida, Vo_medida, color = 'black', label='Medición', linewidth=2.5)
 
-plt.xlabel(f'$V_{{reg}} [V]$')
+plt.xlabel(f'$V_\mathrm{{REG}} \quad [V]$')
 plt.xlim(np.min(Vreg_medida), np.max(Vreg_medida))
-plt.axvline(x = 6, linestyle = '--', color='red', linewidth=1, label='Vreg = 6V', alpha = 0.5)
-plt.text(6.5, plt.ylim()[1]*0, '6V', ha='center', fontsize=9, color='red')
+plt.axvline(x = 6, linestyle = '--', color='red', linewidth=1, label=f'$V_\mathrm{{REG}}^\mathrm{{min}}|_{{V_{{O}} = 5\,\mathrm{{V}}}}$', alpha = 0.5)
+plt.text(6.5, 0.5, '6 V', ha='center', fontsize=9, color='red')
 plt.legend()
 
-plt.ylabel(f'$V_{{o}} [V]$')
+plt.ylabel(f'$V_{{O}} \quad [V]$')
 
 plt.title("Regulación de Línea")
 plt.grid()
+plt.savefig("/Users/lautarogarciavitale/Desktop/UBA/Materias/CIRCUITOS 2/taller-de-circuitos-grupo-5/Checkpoint 3/Códigos informe/reg_lin.png", dpi=300, bbox_inches='tight')
 plt.show()
-plt.savefig("Regulacion_de_Linea.png")
